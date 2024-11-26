2e842c2b169ed3263c033fd4ebaa06a5b9042732
NikIan-1906 <nikian1906@gmail.com>
2024-11-26 09:22:46 +0300

    Cap reading done + get one frame for analysis

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import cv2
 
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if cv2.waitKey(33) & 0xFF == 32:
+        img = frame
+        cv2.imshow("img", img)
+
+    if cv2.waitKey(33) & 0xFF == ord("q") or not ret:
+        break
+
+    cv2.imshow("frame", frame)
+
+
+cap.release()
+cv2.destroyAllWindows()
